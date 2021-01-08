56c1fefa8f8d735232b040f0e12f5fccbbee2a99
ochi <mune.ochi@gmail.com>
2021-01-08 19:51:42 +0900

    Jetpack Navigation を導入

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # the Affirmations app
 - [Display a scrollable list](https://developer.android.com/courses/pathways/android-basics-kotlin-unit-2-pathway-2)
+- [Navigation](https://developer.android.com/guide/navigation?hl=ja)
---
+++ app/src/main/java/com/example/affirmations/BlankFragment.kt
@@ -0,0 +1,46 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(requireActivity(), myDataset)
+
+        recyclerView.setHasFixedSize(true)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment BlankFragment.
+         */
+        @JvmStatic
+        fun newInstance() = BlankFragment()
+    }
+}
